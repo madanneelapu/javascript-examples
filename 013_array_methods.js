8d757c6f0891907926cea1c5e91f9943bb1e47cf
madanneelapu <madanneelapu@gmail.com>
2020-02-12 15:00:53 +0530

    array methods

--- 013_array_methods.js
+++ 013_array_methods.js
@@ -17,3 +17,23 @@ names.shift();
 //Returns new length of array
 names.unshift("Balmer");
 
+// add (splice-in) new elements into array.
+// arg1 - index at which the new elements needs to be added
+// arg2 - number of elements to be removed (including element at index in arg1)
+names.splice(2,0,'jennifer','Aston');
+console.log(names);
+
+//Slice-out a piece of array into a new array 
+//elements from the start argument, and up to (but not including) the end argument.
+//Slice does not remove any elements from source array
+var names_2 = names.slice(4);
+console.log(names_2);
+var names_3 = names.slice(4,6);
+console.log(names_3);
+
+//merging arrays
+var arr1 = ["Cecilie", "Lone"];
+var arr2 = ["Emil", "Tobias", "Linus"];
+var arr3 = ["Robin", "Morgan"];
+var myChildren = arr1.concat(arr2, arr3);   // Concatenates arr1 with arr2 and arr3
+console.log(myChildren);

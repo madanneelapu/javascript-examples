6938511251f1472a240fe2424ed9f8132d3adc24
Madan Neelapu <madanneelapu@gmail.com>
2022-04-29 12:27:14 +0530

    Update 003_typeinterrogate.js

--- 003_typeinterrogate.js
+++ 003_typeinterrogate.js
@@ -1,14 +1,14 @@
 var a; 
-console.log(typeof(a));
+console.log(typeof(a)); // Undefined
 
 a = 10; 
-console.log(typeof(a));
+console.log(typeof(a)); //Number
 
 a = "Sajix"; 
-console.log(typeof(a));
+console.log(typeof(a)); //String
 
 a = true; 
-console.log(typeof(a));
+console.log(typeof(a)); //Boolean
 
 a = null; 
-console.log(typeof(a));
+console.log(typeof(a)); // Object

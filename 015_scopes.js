02dd4e611d606de7e00ca9bc3853811b4b19d802
Madan Neelapu <madanneelapu@gmail.com>
2022-04-29 14:50:13 +0530

    Update 015_scopes.js

--- 015_scopes.js
+++ 015_scopes.js
@@ -3,10 +3,11 @@ function myFn(){
   
   if(a==10){   
     var msg = "value of a is 10"; //Block - level scoping
-    console.log(msg);
+    console.log(msg); // value of a is 10
   }
    
   console.log(msg); //msg will be hoisted to Function level
+  // value of a is 10
 }
 
 myFn();

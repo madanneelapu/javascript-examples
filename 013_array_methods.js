fbef638d1a85d328635e86c86b11320ce3703517
Madan Neelapu <madanneelapu@gmail.com>
2022-04-29 14:44:40 +0530

    Update 013_array_methods.js

--- 013_array_methods.js
+++ 013_array_methods.js
@@ -21,19 +21,19 @@ names.unshift("Balmer");
 // arg1 - index at which the new elements needs to be added
 // arg2 - number of elements to be removed (including element at index in arg1)
 names.splice(2,0,'jennifer','Aston');
-console.log(names);
+console.log(names); // ['john', 'mike', 'jennifer', 'Aston', 'jim', 'bill', 'steve', 'mark', 'gorge', 'kim']
 
 //Slice-out a piece of array into a new array 
 //elements from the start argument, and up to (but not including) the end argument.
 //Slice does not remove any elements from source array
 var names_2 = names.slice(4);
-console.log(names_2);
+console.log(names_2); // ['steve', 'mark', 'gorge', 'kim']
 var names_3 = names.slice(4,6);
-console.log(names_3);
+console.log(names_3); // ['steve', 'mark']
 
 //merging arrays
 var arr1 = ["Cecilie", "Lone"];
 var arr2 = ["Emil", "Tobias", "Linus"];
 var arr3 = ["Robin", "Morgan"];
 var myChildren = arr1.concat(arr2, arr3);   // Concatenates arr1 with arr2 and arr3
-console.log(myChildren);
+console.log(myChildren); // ['Cecilie', 'Lone', 'Emil', 'Tobias', 'Linus', 'Robin', 'Morgan']
